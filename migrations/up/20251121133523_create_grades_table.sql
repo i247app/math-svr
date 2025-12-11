6702018f4be95a5efca901b4df1aad17a140030b
Trần Phước Anh Quốc <anhquoc18092003@gmail.com>
2025-12-11 23:54:08 +0700

    update migrations file

--- migrations/up/20251121133523_create_grades_table.sql
+++ migrations/up/20251121133523_create_grades_table.sql
@@ -16,8 +16,8 @@ CREATE TABLE grades (
 
 -- comment it if you migrate-up again
 -- INSERT INTO grades (id,label,discription,image_key,status,display_order,create_id,create_dt,modify_id,modify_dt,deleted_dt) VALUES
--- 	 ('82023de6-8d1f-46d3-abc8-6dceab23a9f5','Grade 4','Four year of elementary education level.','grade/20251204-44563688-3d06-45e8-8d12-a82b0026c4c2.jpeg','ACTIVE',4,0,'2025-12-04 22:25:54.732',0,'2025-12-04 22:25:54.732',NULL),
--- 	 ('c95bf9eb-7143-4395-9112-752d7aee8020','Grade 2','Second year of elementary education level.','grade/20251204-b25fcde1-fd91-4afc-8dcf-3cc48c636d32.jpg','ACTIVE',2,0,'2025-12-04 22:25:01.597',0,'2025-12-04 22:25:01.597',NULL),
--- 	 ('ca93947f-f7b6-433e-968f-a7b70f36c201','Grade 5','Five year of elementary education level.','grade/20251204-3f6ada77-324a-4d52-b5b3-1253ef4e639b.jpg','ACTIVE',5,0,'2025-12-04 22:26:22.351',0,'2025-12-04 22:26:22.351',NULL),
--- 	 ('d26786b6-7a0a-49c9-ba89-866a4ba55e19','Grade 3','Third year of elementary education level.','grade/20251204-756f7405-1e22-40a9-9e64-9927c9a7acb4.jpeg','ACTIVE',3,0,'2025-12-04 22:25:18.914',0,'2025-12-04 22:25:18.914',NULL),
 -- 	 ('d46c8252-06a7-4d6e-8f24-3525278214ae','Grade 1','First year of elementary education level.','grade/20251204-ee1b0b0b-39eb-494a-a639-5e8641971f42.jpg','ACTIVE',1,0,'2025-12-04 22:24:23.387',0,'2025-12-04 22:24:23.387',NULL);
+-- 	 ('c95bf9eb-7143-4395-9112-752d7aee8020','Grade 2','Second year of elementary education level.','grade/20251204-b25fcde1-fd91-4afc-8dcf-3cc48c636d32.jpg','ACTIVE',2,0,'2025-12-04 22:25:01.597',0,'2025-12-04 22:25:01.597',NULL),
+-- 	 ('d26786b6-7a0a-49c9-ba89-866a4ba55e19','Grade 3','Third year of elementary education level.','grade/20251204-756f7405-1e22-40a9-9e64-9927c9a7acb4.jpeg','ACTIVE',3,0,'2025-12-04 22:25:18.914',0,'2025-12-04 22:25:18.914',NULL),
+-- 	 ('82023de6-8d1f-46d3-abc8-6dceab23a9f5','Grade 4','Four year of elementary education level.','grade/20251204-44563688-3d06-45e8-8d12-a82b0026c4c2.jpeg','ACTIVE',4,0,'2025-12-04 22:25:54.732',0,'2025-12-04 22:25:54.732',NULL),
+-- 	 ('ca93947f-f7b6-433e-968f-a7b70f36c201','Grade 5','Five year of elementary education level.','grade/20251204-3f6ada77-324a-4d52-b5b3-1253ef4e639b.jpg','ACTIVE',5,0,'2025-12-04 22:26:22.351',0,'2025-12-04 22:26:22.351',NULL),
--- migrations/up/20251207230400_create_semesters_table.sql
+++ migrations/up/20251207230400_create_semesters_table.sql
@@ -16,15 +16,8 @@ CREATE TABLE semesters (
 
 
 -- INSERT INTO semesters (id, name, description) VALUES
--- ('2c0h1d3e-3f4g-6e5d-0h2c-9d8e7f6g5c13', 'Midterm 1', 'A first midterm period.'),
--- ('4e2j3f5g-5h6i-8g7f-2j4e-1f0g9h8i7e35', 'End of term 1', 'The first learning period of the academic year.'),
--- ('3d1i2e4f-4g5h-7f6e-1i3d-0e9f8g7h6d24', 'Midterm 2', 'A second midterm period.'),
--- ('5f3k4g6h-6i7j-9h8g-3k5f-2g1h0i9j8f46', 'End of term 2', 'The second learning period of the academic year.'),
-
--- -- vietnamese
--- ('7g4l5h7i-7j8k-0i9h-4l6g-3h2i1j0k9g58', 'Giữa kỳ 1', 'Kỳ thi giữa kỳ đầu tiên.'),
--- ('8h5m6i8j-8k9l-1j0i-5m7h-4i3j2k1l0h69', 'Cuối kỳ 1', 'Kết thúc học kỳ đầu tiên của năm học.'),
--- ('9i6n7j9k-9l0m-2k1j-6n8i-5j4k3l2m1i70', 'Giữa kỳ 2', 'Kỳ thi giữa kỳ thứ hai.'),
--- ('0j7o8k0l-0m1n-3l2k-7o9j-6k5l4m3n2j81', 'Cuối kỳ 2', 'Kết thúc học kỳ thứ hai của năm học.');
-
+-- ('2c0h1d3e-3f4g-6e5d-0h2c-9d8e7f6g5c13', 'Semester 1', 'Semester 1 program'),
+-- ('4e2j3f5g-5h6i-8g7f-2j4e-1f0g9h8i7e35', 'Semester 2', 'Semester 2 program'),
+-- ('3d1i2e4f-4g5h-7f6e-1i3d-0e9f8g7h6d24', 'Semester 3', 'Semester 3 program'),
+-- ('5f3k4g6h-6i7j-9h8g-3k5f-2g1h0i9j8f46', 'Semester 4', 'Semester 4 program'),
 
