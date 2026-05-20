055f765a2de66a33b6ced47012c40c67d3e6f2b7
Trần Phước Anh Quốc <anhquoc18092003@gmail.com>
2026-05-20 15:46:52 +0700

    update user service

--- internal/bootstrap/routes/routes.go
+++ internal/bootstrap/routes/routes.go
@@ -28,7 +28,7 @@ func SetupHttpRoutes(gexSvr *gex.Server, res *resource.Resource, services *conta
 	{
 		userHandler := user.NewUserHandler(res, services.UserSvc)
 		gexSvr.AddRoute("GET  /users/{id}", userHandler.HandleGetUserById)
-		gexSvr.AddRoute("GET  /users/me", userHandler.HandleGetUserMe)
+		gexSvr.AddRoute("POST /users/me", userHandler.HandleGetUserMe)
 		gexSvr.AddRoute("POST /users/list", userHandler.HandleListUsers)
 		gexSvr.AddRoute("POST /users/create", userHandler.HandleCreateUser)
 		gexSvr.AddRoute("POST /users/update", userHandler.HandleUpdateUser)
