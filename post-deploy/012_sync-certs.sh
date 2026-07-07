0eb992f4ecd2fa2f0016f0775a9287f5fc0401cb
rkl550 <rkl550@gmail.com>
2026-07-08 00:04:46 +0700

    quickfix sync ssl certs

--- post-deploy/012_sync-certs.sh
+++ post-deploy/012_sync-certs.sh
@@ -4,14 +4,16 @@
 
 pwd
 
-for n in {0..5}
+#for n in {0..5}
+for n in {0}
 do
   src_dir="/etc/letsencrypt/live/i247.com"
   tgt_dir="/apps/math/keys"
 
   if sudo test -f ${src_dir:-notset}/fullchain.pem
   then
-    echo "syncing t${n} fullchain.pem..."
+    # echo "syncing t${n} fullchain.pem..."
+    echo "syncing ${src_dir}/fullchain.pem..."
 
     sudo rsync -rLti --chown=mot:mot --chmod=400 ${src_dir}/fullchain.pem ${tgt_dir}/
 
@@ -19,7 +21,8 @@ do
 
   if sudo test -f ${src_dir:-notset}/privkey.pem
   then
-    echo "syncing t${n} privkey.pem..."
+    # echo "syncing t${n} privkey.pem..."
+    echo "syncing ${src_dir}/privkey.pem..."
 
     sudo rsync -rLti --chown=mot:mot --chmod=400 ${src_dir}/privkey.pem ${tgt_dir}/
 
