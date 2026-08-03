643e3115af8e0de91eba1d7cabce8f6e7c037bf6
Trần Phước Anh Quốc <anhquoc18092003@gmail.com>
2026-08-03 17:43:02 +0700

    update banner table to migration file

--- migrations/022_ma_banners.sql
+++ migrations/022_ma_banners.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS ma_banners  (
   id                BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   banner_id         BIGINT UNSIGNED NOT NULL UNIQUE,
-  title_text        VARCHAR(255) DEFAULT NULL,
+  title             VARCHAR(255) DEFAULT NULL,
   short_text        VARCHAR(1000) DEFAULT NULL,
   media_type        VARCHAR(32) NOT NULL DEFAULT 'IMAGE', -- 'TEXT' or 'IMAGE' or 'VIDEO'
-  media_url         VARCHAR(1000) NOT NULL,
+  media_url_key     VARCHAR(1000) NOT NULL,
   button_text       VARCHAR(255) DEFAULT NULL,
   button_link_url   VARCHAR(1000) DEFAULT NULL,
   note              VARCHAR(500) DEFAULT NULL,
@@ -14,5 +14,5 @@ CREATE TABLE IF NOT EXISTS ma_banners  (
   create_dt         DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
   modify_id         BIGINT UNSIGNED DEFAULT NULL,
   modify_dt         DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-  deleted_dt        DATETIME(6) DEFAULT NULL,
+  deleted_dt        DATETIME(6) DEFAULT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
