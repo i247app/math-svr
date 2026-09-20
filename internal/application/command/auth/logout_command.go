acda83d54328c6cb5e22a790d11b0aca4b9336eb
Trần Phước Anh Quốc <anhquoc18092003@gmail.com>
2026-09-20 11:38:07 +0700

    update auth module

--- internal/application/command/auth/logout_command.go
+++ internal/application/command/auth/logout_command.go
@@ -4,8 +4,6 @@ import (
 	"context"
 
 	"math-ai.com/math-ai/internal/application/transaction"
-	errs "math-ai.com/math-ai/internal/domain/shared/error"
-	"math-ai.com/math-ai/internal/domain/shared/status"
 	"math-ai.com/math-ai/internal/shared/enum"
 )
 
@@ -24,18 +22,23 @@ func NewLogoutCommandHandler(uow transaction.UnitOfWork) *LogoutCommandHandler {
 
 func (h *LogoutCommandHandler) Handle(ctx context.Context, cmd LogoutCommand) error {
 	return h.uow.Do(ctx, func(ctx context.Context, repos transaction.Repositories) error {
-		ll, err := repos.LoginLog.FindActiveByUserDevice(ctx, cmd.UserID, cmd.DeviceUUID)
-		if err != nil {
-			return errs.NewError(ctx, status.AUTH_LOGOUT_FAILED, nil, err)
-		}
-		if ll == nil {
-			return errs.NewError(ctx, status.AUTH_INVALID_TOKEN, nil, ErrTokenNotFoundOrRevoked)
-		}
+		// ll, err := repos.LoginLog.FindActiveByUserDevice(ctx, cmd.UserID, cmd.DeviceUUID)
+		// if err != nil {
+		// 	return errs.NewError(ctx, status.AUTH_LOGOUT_FAILED, nil, err)
+		// }
+		// if ll == nil {
+		// 	return errs.NewError(ctx, status.AUTH_INVALID_TOKEN, nil, ErrTokenNotFoundOrRevoked)
+		// }
 
-		if err := repos.LoginLog.MarkStatusByLoginLogId(
-			ctx, ll.LoginLogId(), enum.LoginLogStatusTypeRevoked,
-		); err != nil {
-			return errs.NewError(ctx, status.AUTH_LOGOUT_FAILED, nil, err)
+		// if err := repos.LoginLog.MarkStatusByLoginLogId(
+		// 	ctx, ll.LoginLogId(), enum.LoginLogStatusTypeRevoked,
+		// ); err != nil {
+		// 	return errs.NewError(ctx, status.AUTH_LOGOUT_FAILED, nil, err)
+		// }
+
+		err := repos.LoginLog.MarkStatusByUserDevice(ctx, cmd.UserID, cmd.DeviceUUID, enum.LoginLogStatusTypeRevoked)
+		if err != nil {
+			return err
 		}
 		return nil
 	})
